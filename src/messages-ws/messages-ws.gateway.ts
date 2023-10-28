631d18c0be17a2d39f9b94a8c1b3a8138e042fa0
Richard Rodriguez <richardmanuel811@gmail.com>
2023-10-27 22:38:55 -0500

    - finishing 15 class

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,6 +2,8 @@ import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGa
 import { MessagesWsService } from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
 import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -9,12 +11,22 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   @WebSocketServer() wss: Server
   
   constructor(
-      private readonly messagesWsService: MessagesWsService
+      private readonly messagesWsService: MessagesWsService,
+      private readonly jwtService: JwtService
     ) {}
 
 
-  handleConnection(client: Socket) {
-    this.messagesWsService.registerClient(client)
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token)
+      await this.messagesWsService.registerClient(client, payload.id)
+
+    } catch (error) {
+      client.disconnect()
+      return
+    }
 
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients())
 
@@ -45,7 +57,7 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
     // })
 
     this.wss.emit('messages-from-server', {
-        fullName: 'asdasdas',
+        fullName:this.messagesWsService.getUserFull(client.id),
         message: payload.message || 'no-message'
       })
 
